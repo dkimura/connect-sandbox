9fed6a1a2e8299c6592ffd6f69738b560dc978e0
dkimura <dkimura@bdd.xyz>
2024-10-06 23:58:49 +0900

    fix(page.tsx): update useQuery parameters to match expected format for userId to ensure correct data fetching

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { getUser } from "~/__generated__/user-UserService_connectquery";
 
 export default function Home() {
-  const { data } = useQuery(getUser.useQuery({ userId: 1 }));
+  const { data } = useQuery(getUser, { userId: "1" });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
